27035a53d4b76c0d04a0e671b2452d42e493e265
Dara Heaphy <23369914@studentmail.ul.ie>
2025-01-29 21:01:54 +0000

    committing main and flight temporarily

--- src/Flight.java
+++ src/Flight.java
@@ -1,6 +1,7 @@
 public abstract class Flight {
     private String flightNumber;
     private FlightState state;
+    private int fuel = 100;
 
     public String getFlightNumber() {
         return flightNumber;
@@ -22,6 +23,7 @@ public abstract class Flight {
 
     public void takeOff() {
         state.takeOff(this);
+        consumeFuel();
     }
 
     public void land() {
@@ -30,5 +32,17 @@ public abstract class Flight {
     
     public void hold() {
         state.hold(this);
+    }
+
+    public int getFuel() {
+        return fuel;
+    }
+
+    public void consumeFuel() {
+        fuel -= 10;
+    }
+
+    public void setFuel(int newFuel) {
+        this.fuel = newFuel;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        Flight privateFlight = FlightFactory.createFlight(FlightType.PRIVATE, "PF001");
-        System.out.println("flight type is: " + privateFlight.getType());
-        System.out.println("flight number is: " + privateFlight.getFlightNumber());
+        Flight basicFlight = FlightFactory.createFlight(FlightType.PRIVATE, "PF001");
+        FuelDecorator privateFlight = new FuelDecorator(basicFlight);
+        
+        privateFlight.setFuelConsumptionRate(15);
+        
+        System.out.println("Flight type is: " + privateFlight.getType());
+        System.out.println("Flight number is: " + privateFlight.getFlightNumber());
         System.out.println("Initial state is: " + privateFlight.getState());
+        System.out.println("Initial fuel: " + privateFlight.getFuel());
 
         privateFlight.takeOff();
         System.out.println("Current state is: " + privateFlight.getState());
+        System.out.println("Fuel after takeoff: " + privateFlight.getFuel());
 
         privateFlight.hold();
         privateFlight.land();
         System.out.println("Current state is: " + privateFlight.getState());
 
         privateFlight.land();
-
         System.out.println("Current state is: " + privateFlight.getState());
-        privateFlight.land();
-
     }
 }
