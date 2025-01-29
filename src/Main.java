ade21e935f5e6ce8851918bc9fce28cbec5642b7
Tom J <thomasjdjoyce@gmail.com>
2025-01-29 18:36:45 +0000

    Added state pattern, this keeps track of where the plane is and thus limits its behaviour based on current state.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 public class Main {
     public static void main(String[] args) {
         Flight privateFlight = FlightFactory.createFlight(FlightType.PRIVATE, "PF001");
-        System.out.println(privateFlight.getType());
-        System.out.println(privateFlight.getFlightNumber());
+        System.out.println("flight type is: " + privateFlight.getType());
+        System.out.println("flight number is: " + privateFlight.getFlightNumber());
+        System.out.println("Initial state is: " + privateFlight.getState());
+
+        privateFlight.takeOff();
+        System.out.println("Current state is: " + privateFlight.getState());
+
+        privateFlight.hold();
+        privateFlight.land();
+        System.out.println("Current state is: " + privateFlight.getState());
+
+        privateFlight.land();
+
+        System.out.println("Current state is: " + privateFlight.getState());
+        privateFlight.land();
+
     }
 }
