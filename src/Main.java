a0c9524357c7d738d8395282de123ab969cd9500
Dara Heaphy <23369914@studentmail.ul.ie>
2025-01-29 21:27:09 +0000

    restoring main branch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
         Flight basicFlight = FlightFactory.createFlight(FlightType.PRIVATE, "PF001");
         FuelDecorator privateFlight = new FuelDecorator(basicFlight);
         
@@ -21,26 +20,5 @@ public class Main {
 
         privateFlight.land();
         System.out.println("Current state is: " + privateFlight.getState());
-=======
-        WeatherStation weatherStation = new WeatherStation();
-        
-        Flight privateFlight = FlightFactory.createFlight(FlightType.PRIVATE, "PF001");
-        Flight passengerFlight = FlightFactory.createFlight(FlightType.PASSENGER, "PA001");
-        Flight militaryFlight = FlightFactory.createFlight(FlightType.MILITARY, "MF001");
-        
-        // Take off some flights
-        privateFlight.takeOff();
-        passengerFlight.takeOff();
-        militaryFlight.takeOff();
-        
-        System.out.println("\n=== Weather Updates ===");
-        weatherStation.reportSunny("Visibility excellent, ceiling unlimited");
-        weatherStation.reportStorm("Severe thunderstorm approaching");
-        
-        System.out.println("\n=== Checking States ===");
-        System.out.println("Private flight: " + privateFlight.getState());
-        System.out.println("Passenger flight: " + passengerFlight.getState());
-        System.out.println("Military flight: " + militaryFlight.getState());
->>>>>>> a7c100bad2b5cac0fe9dc6ef18099c7f45ff1220
     }
 }
