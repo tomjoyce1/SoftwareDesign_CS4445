0e2724716f231d8c08f75e7b4a355c43b05ed7e9
Nanda <nandakishorevinay@gmail.com>
2025-02-07 17:53:15 +0000

    Refactor classes to use final fields for immutability and simplify FlightFactory with enhanced switch expression

--- src/main/java/FlightFactory.java
+++ src/main/java/FlightFactory.java
@@ -1,16 +1,10 @@
 public class FlightFactory {
     public static Flight createFlight(FlightType type, String flightNumber) {
-        switch (type) {
-            case PRIVATE:
-                return new PrivateFlight(flightNumber);
-            case PASSENGER:
-                return new PassengerFlight(flightNumber);
-            case MILITARY:
-                return new MilitaryFlight(flightNumber);
-            case CARGO:
-                return new CargoFlight(flightNumber);
-            default:
-                return null;
-        }
+        return switch (type) {
+            case PRIVATE -> new PrivateFlight(flightNumber);
+            case PASSENGER -> new PassengerFlight(flightNumber);
+            case MILITARY -> new MilitaryFlight(flightNumber);
+            case CARGO -> new CargoFlight(flightNumber);
+        };
     }
 }
