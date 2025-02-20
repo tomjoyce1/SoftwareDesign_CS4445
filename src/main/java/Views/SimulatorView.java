67de2c02e013f84458b0b889c58967dad8096426
Dara Heaphy <23369914@studentmail.ul.ie>
2025-02-20 01:06:34 +0000

    making previous PR again

---
+++ src/main/java/Views/SimulatorView.java
@@ -0,0 +1,39 @@
+package Views;
+
+import java.util.Scanner;
+import Models.Decorators.RadarDecorator.RadarDisplay;
+
+public class SimulatorView {
+    private final Scanner scanner;
+    private final RadarDisplay radar;
+
+    public SimulatorView(RadarDisplay radar) {
+        this.radar = radar;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void displayMenu() {
+        System.out.println("\n=== Main Menu ===");
+        System.out.println("1. Create new flight");
+        System.out.println("2. Control flight");
+        System.out.println("3. Update weather");
+        System.out.println("4. List all flights");
+        System.out.println("5. Check Flight Status");
+        System.out.println("Q. Quit");
+        System.out.print("Choose an option: ");
+    }
+
+    // method to replace all other instances of scanner.nextLine() in accordance with MVC arc pattern best practices
+    public String getUserInput() {
+        return scanner.nextLine().toUpperCase();
+    }
+
+    public void displayMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void displayRadar() {
+        System.out.println("\n--- Radar Display ---");
+        radar.show();
+    }
+}
